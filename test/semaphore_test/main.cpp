03c7322ec412252e4ae1471845ac144dda201469
SulfurNitride <SulfurNitride@users.noreply.github.com>
2026-08-01 01:25:48 -0500

    Test recursive benaphore exclusion

---
+++ test/semaphore_test/main.cpp
@@ -0,0 +1,61 @@
+#include "semaphore.h"
+
+#include <gtest/gtest.h>
+
+#include <chrono>
+#include <future>
+#include <thread>
+
+using namespace std::chrono_literals;
+
+TEST(RecursiveBenaphoreTest, RecursiveOwnerExcludesAnotherThread)
+{
+  RecursiveBenaphore mutex;
+  std::promise<void> ownerAcquired;
+  std::promise<void> releaseOnce;
+  std::promise<void> releasedOnce;
+  std::promise<void> releaseFinally;
+  std::promise<void> waiterStarted;
+  std::promise<void> waiterAcquired;
+
+  auto ownerAcquiredFuture = ownerAcquired.get_future();
+  auto releaseOnceFuture = releaseOnce.get_future();
+  auto releasedOnceFuture = releasedOnce.get_future();
+  auto releaseFinallyFuture = releaseFinally.get_future();
+  auto waiterStartedFuture = waiterStarted.get_future();
+  auto waiterAcquiredFuture = waiterAcquired.get_future();
+
+  std::thread owner([&]() {
+    mutex.wait();
+    mutex.wait();
+    ownerAcquired.set_value();
+
+    releaseOnceFuture.wait();
+    mutex.signal();
+    releasedOnce.set_value();
+
+    releaseFinallyFuture.wait();
+    mutex.signal();
+  });
+
+  ownerAcquiredFuture.wait();
+  std::thread waiter([&]() {
+    waiterStarted.set_value();
+    mutex.wait();
+    waiterAcquired.set_value();
+    mutex.signal();
+  });
+
+  waiterStartedFuture.wait();
+  EXPECT_EQ(waiterAcquiredFuture.wait_for(250ms), std::future_status::timeout);
+
+  releaseOnce.set_value();
+  releasedOnceFuture.wait();
+  EXPECT_EQ(waiterAcquiredFuture.wait_for(250ms), std::future_status::timeout);
+
+  releaseFinally.set_value();
+  EXPECT_EQ(waiterAcquiredFuture.wait_for(2s), std::future_status::ready);
+
+  owner.join();
+  waiter.join();
+}
